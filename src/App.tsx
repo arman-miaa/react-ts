c86e4e4e8a2ab2733c233638705ac7f608c715d2
Arman <mdarman365506@gmail.com>
2025-02-17 08:17:33 +0600

    added type for array and object

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@
 import './App.css';
 import User from './components/User';
 
+const user1 = {
+  name: "Abdullah",
+  age: 24,
+  isRegistered: false,
+  lang: ['Bangla', 'Arabic', 'English'],
+}
+
 function App() {
   return (
     <div className="App">
       <h1>React Typescript</h1>
-      <User name='arman-mia' age={27} isRegistered={true} />
-      <User name='Shohidullah' age={26} isRegistered={false} />
+      <User name='arman-mia' age={27} isRegistered={true} lang={['Arabic','English', 'Bangla']} user={user1} />
+      {/* <User name='Shohidullah' age={26} isRegistered={false} /> */}
     </div>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,15 +1,26 @@
+type UserProps = {
+  name: string;
+  age: number;
+  isRegistered: boolean;
+  lang: string[];
+  user: { name: string; age: number; isRegistered: boolean; lang: string[] };
+};
 
-type UserProps = { name: string; age: number;  isRegistered: boolean}
-
-const User = ({name,age,isRegistered}: UserProps) => {
+const User = ({ name, age, isRegistered,lang,user }: UserProps) => {
   return (
-      <div>
-          <h2>{name}</h2>
-          <p>{age} years old</p>
-          {isRegistered ? <p>Student Registered</p> : <p>Student not registered</p>}
-         
+    <div>
+      <h2>{name}</h2>
+      <p>{age} years old</p>
+      {isRegistered ? <p>Student Registered</p> : <p>Student not registered</p>}
+      Speak In...
+      {lang.map((language, index) => (
+        <span key={index}> {language}</span>
+      ))}
+      <p>{user.name}</p>
+      <p>{user.age}</p>
+     
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
