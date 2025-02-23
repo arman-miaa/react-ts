dcee9a1442f105f7ccc6aba4f725a83d2f3a8403
Arman <mdarman365506@gmail.com>
2025-02-23 11:56:31 +0600

    added typing events

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 
-import { useState } from 'react';
-import './App.css';
 
-type User = {
-  id: number,
-  name: string,
-}
+import './App.css';
+import NewUser from './components/NewUser';
+
+
 
 function App() {
-  const [count, setCount] = useState<null | User>(null);
+ 
 
-  const handleCount = () => {
-    setCount({ id: 1, name: "Arman" });
-    console.log(count);
-  }
+
 
   return (
     <div className='App'>
-      <h1>{ count?.name}</h1>
-      <button onClick={handleCount}>Increment</button>
+      <NewUser/>
     </div>
   );
 }
